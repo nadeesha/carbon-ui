9b68298088ab7f2ba2aaff73b33da024f0592cd4
Nadeesha Cabral <nadeesha.cabral@gmail.com>
2014-05-16 09:01:49 +0530

    init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('carbonUiApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
